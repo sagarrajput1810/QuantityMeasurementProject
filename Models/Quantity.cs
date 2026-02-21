1ec5524aecc89de45e25fff51ceeb69027f52d7a
Sagar Rajput <sagarrajput1810@gmail.com>
2026-02-21 15:25:49 +0530

    UC1 Complete

---
+++ Models/Quantity.cs
@@ -0,0 +1,19 @@
+
+namespace QuantityMeasurementSystem.Models
+{
+    public class Quantity
+    {
+        public double Value{get;set;}
+        public Quantity(double value)
+        {
+            Value = value;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(!(obj is Quantity)) return false;
+            Quantity other = (Quantity) obj;
+            return Value == other.Value;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using QuantityMeasurementSystem.Models;
+
+// Do alag objects banaye same value ke
+Quantity q1 = new Quantity(0.0);
+Quantity q2 = new Quantity(0.0);
+
+// Check if they are equal
+bool result = q1.Equals(q2);
+
+Console.WriteLine("--- UC 1: Feet Equality Test ---");
+Console.WriteLine($"Are 0.0 and 0.0 equal? {result}");
