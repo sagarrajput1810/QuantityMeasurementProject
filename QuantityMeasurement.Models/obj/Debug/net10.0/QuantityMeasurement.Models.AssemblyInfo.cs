1c501fb13135d63cafb55d36d158a8be0d6dc45e
Sagar Rajput <sagarrajput1810@gmail.com>
2026-04-16 12:09:26 +0530

    done

--- QuantityMeasurement.Models/obj/Debug/net10.0/QuantityMeasurement.Models.AssemblyInfo.cs
+++ QuantityMeasurement.Models/obj/Debug/net10.0/QuantityMeasurement.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuantityMeasurement.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2a18487907abb230610198cdac49aeb3357a309")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eebf524d6282680156d77bc82216bd2ed5150ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuantityMeasurement.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuantityMeasurement.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
